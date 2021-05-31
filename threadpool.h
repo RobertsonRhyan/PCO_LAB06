9eaba9ee11b0dd244936986b1c857856399b5665
rhyan <rhyan.robertson@heig-vd.ch>
2021-05-31 03:41:09 +0200

    Working code

--- threadpool.h
+++ threadpool.h
@@ -40,20 +40,30 @@ class ThreadPool
 {
 
 public:
-    ThreadPool(int maxThreadCount, int maxNbWaiting): stop(false), nbWaitingThread(0), threadCount(0) {
+    ThreadPool(unsigned maxThreadCount, unsigned maxNbWaiting): stop(false), nbWaitingThread(0), nbWaitingRunnable(0), threadCount(0) {
         this->maxThreadCount = maxThreadCount;
         this->maxNbWaiting = maxNbWaiting;
 
     }
 
     ~ThreadPool(){
-        stop = true;
-        waitForRunnable.notifyAll();
 
-        /*
+        stop = true;
+
+        // While there is waiting threads, notify them to continue.
+        while (nbWaitingThread > 0) {
+            mutex.unlock(); // Seems to help pass tests - Need to investigate
+
+            // Notify all waiting threads to continue
+            waitForRunnable.notifyAll();
+        }
+
+        // Wait for all threads to terminate
         for(auto& thread: threads){
                 thread->join();
-        }*/
+                delete thread;
+        }
+
     }
 
 
@@ -70,19 +80,50 @@ public:
     bool start(Runnable* runnable) {
         mutex.lock();
 
+        // If stop requested, do not run runnable.
+        if(stop){
+            mutex.unlock();
+            return false;
+        }
+
+        // If 1 or more available threads
         if(nbWaitingThread > 0){
+            // Push runnable on queue
             runnableQueue.push(runnable);
+            // Wake a waiting thread
             waitForRunnable.notifyOne();
-        }else if(threadCount < maxThreadCount){
-            threads.push_back(new PcoThread (&ThreadPool::processRunnable, this));
+        }
+        // If no available threads but still below thread count threshold
+        else if(threadCount < maxThreadCount){
+            // Create a new thread and insert into threads vector
+            threads.push_back(new PcoThread (&ThreadPool::processRunnable,
+                                             this));
             ++threadCount;
+
+            // Add runnable to queue
             runnableQueue.push(runnable);
+            // Wake a waiting thread
             waitForRunnable.notifyOne();
-        }else if(runnableQueue.size() < maxNbWaiting){
+        }
+        // If no available threads but still below
+        // waiting runnable count threshold
+        else if(nbWaitingRunnable < maxNbWaiting){
+
+            ++nbWaitingRunnable;
+            // Block caller until a thread becomes available again
+            while(!stop && nbWaitingThread < 1){
+                waitForThread.wait(&mutex);
+            }
+
             runnableQueue.push(runnable);
-            waitForThread.wait(&mutex);
+            --nbWaitingRunnable;
             waitForRunnable.notifyOne();
-        }else{
+        }
+        // If the amount of waiting runnables has reached the threshold,
+        // cancel the runnable and return false
+        else{
+            runnable->cancelRun();
+            mutex.unlock();
             return false;
         }
 
@@ -92,40 +133,49 @@ public:
 
 private:
     bool stop;
-    int nbWaitingThread;
-    int threadCount;
-    int maxThreadCount;
-    int maxNbWaiting;
+    unsigned nbWaitingThread;
+    unsigned nbWaitingRunnable;
+    unsigned threadCount;
+    unsigned maxThreadCount;
+    unsigned maxNbWaiting;
 
     PcoMutex mutex;
-    std::queue<Runnable *> runnableQueue;
-
-    std::vector<PcoThread *> threads;
     PcoConditionVariable waitForRunnable, waitForThread;
+
+    std::queue<Runnable *> runnableQueue;
+    std::vector<PcoThread *> threads;
+    //std::vector<std::unique_ptr<PcoThread> > threads;
+
 
     void processRunnable(){
         Runnable *currentRunnable;
 
-        while (!stop) {
-
+        while (1) {
             mutex.lock();
             ++nbWaitingThread;
-            while ((runnableQueue.size() < 1) && !stop){
+
+            while (!stop && runnableQueue.size() < 1){
                 waitForRunnable.wait(&mutex);
             }
 
-            if(stop)
-                return;
+            --nbWaitingThread;
 
             currentRunnable = runnableQueue.front();
             runnableQueue.pop();
-            --nbWaitingThread;
+
             mutex.unlock();
 
+            if(stop)
+                break;
+
             currentRunnable->run();
+
             waitForThread.notifyOne();
         }
+
     }
+
+
 
 
 };
